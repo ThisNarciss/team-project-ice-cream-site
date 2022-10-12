6543db6bc1b903c35a670562652590c8c5cbeb11
Nazarenko Alex <nazarenko.alx@gmail.com>
2022-10-12 15:05:09 +0300

    adds js

--- src/index.js
+++ src/index.js
@@ -2,5 +2,9 @@ import './js/anchor-scroll';
 import './js/simple-light-box';
 import './js/slick-slider';
 import './js/mobile-menu';
-import './js/modal';
+
+import './js/location-modal';
 import './js/franchise-modal';
+import './js/about-modal';
+import './js/ingredients-modal';
+import './js/buynow-modal';
